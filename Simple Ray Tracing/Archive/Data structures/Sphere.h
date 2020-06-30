1d95431531919aa4fcf3c04edfcf968c19bfed51
JinRSA <66529793+JinRSA@users.noreply.github.com>
2020-06-30 23:36:44 +0400

    Update Sphere.h

--- Simple Ray Tracing/Archive/Data structures/Sphere.h
+++ Simple Ray Tracing/Archive/Data structures/Sphere.h
@@ -3,11 +3,11 @@
 #include "Geometry.h"
 #include "Material.h"
 
-class /*alignas(32)*/ Sphere
+class alignas(32) Sphere
 {
 public:
 	Material material;
-	/*alignas(32) */Vec3f center;
+	alignas(32) Vec3f center;
 	float radius;
 	Sphere() : center(0.f, 0.f, 0.f), radius(1) {};
 	Sphere(const decltype(center)& center, const float& radius, const Material& material) : center(center), radius(radius), material(material) {};
